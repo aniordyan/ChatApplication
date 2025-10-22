462c9415e18826d56023c319b83ffcb0abddfd55
Ani Ordyan <ordyanani@gmail.com>
2025-10-22 23:45:28 +0400

    initial commit

---
+++ mainform.pas
@@ -0,0 +1,39 @@
+unit MainForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Memo1: TMemo;
+    procedure Label1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Label1Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
