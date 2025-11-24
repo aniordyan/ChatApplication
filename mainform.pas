cae24505df0c44caf2c5759ef3aba09f676249e7
Ani Ordyan <ordyanani@gmail.com>
2025-11-24 20:19:26 +0400

    removed synapse lib usage, now it works with default library and (finally (╯°□°）╯︵ ┻━┻) supports ipv6

--- mainform.pas
+++ mainform.pas
@@ -6,36 +6,43 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  blcksock, synsock, IniFiles, uBuddies; // tcp/ip library    + conf file for storing buddies
-
+  Sockets, BaseUnix, IniFiles, uBuddies; // use FPC networking units
 
 type
-  TForm1 = class;  // forward declaration
+  TForm1 = class;
 
 
- type    //recieve data, append to memo
+  TSocketHandle = cint;
+const
+  INVALID_SOCKET = -1;
+
+type
+  // =========================
+  // Receiver thread (incoming data)
+  // =========================
   TReceiverThread = class(TThread)
   private
-    FSocket: TTCPBlockSocket;
+    FSocket: TSocketHandle;
     FMemo: TMemo;
     FBuf: string;
     FLines: TStringList;
     procedure AppendLines;
   public
-    constructor Create(ASocket: TTCPBlockSocket; AMemo: TMemo);
+    constructor Create(ASocket: TSocketHandle; AMemo: TMemo);
     destructor Destroy; override;
     procedure Execute; override;      //main
   end;
 
 
-  { TAcceptorThread }
-  type
+  // =========================
+  // Acceptor thread (server side)
+  // =========================
   TAcceptorThread = class(TThread)
   private
-    FListenSock: TTCPBlockSocket;
+    FListenSock: TSocketHandle;
     FPort: string;
     FOwner: TForm1;
-    FAcceptedSock: TTCPBlockSocket;
+    FAcceptedSock: TSocketHandle;
     procedure SyncAccepted;
   protected
     procedure Execute; override;
@@ -46,10 +53,7 @@ type
   end;
 
 
-
-
   { TForm1 }
-  type
   TForm1 = class(TForm)
     Panel1: TPanel;     // buddies panel
     Label1: TLabel;     // "տնեցիք"
@@ -66,7 +70,7 @@ type
     Button5: TButton;   // "լսել" (Listen)
     Button3: TButton;   // "ուղարկել" (Send)
 
-    lstBuddies: TListBox; // ADD THIS in the designer inside Panel1
+    lstBuddies: TListBox;
 
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -85,20 +89,19 @@ type
     procedure portChange(Sender: TObject);
     procedure Label1Click(Sender: TObject);
   private
-    FSocket: TTCPBlockSocket;
+    FSocket: TSocketHandle;
     FConnected: Boolean;
     FReceiverThread: TReceiverThread;
     FAcceptorThread: TAcceptorThread;
 
-    //FBuddies: array of TBuddy;
     FBuddyManager: TBuddyManager;
 
-    procedure AttachIncomingSocket(ASocket: TTCPBlockSocket);        //
-    procedure UpdateButtons;                                           //no impl
+    procedure AttachIncomingSocket(ASocket: TSocketHandle);
+    procedure UpdateButtons;
 
-    function GetConfigFileName: string;                            //
-    procedure RefreshBuddyList;                                  //
-    procedure ConnectToHost(const Host, PortStr: string);       //
+    function GetConfigFileName: string;
+    procedure RefreshBuddyList;
+    procedure ConnectToHost(const Host, PortStr: string);
   public
   end;
 
@@ -110,11 +113,95 @@ implementation
 
 {$R *.lfm}
 
+{ =========================
+  Helper functions for FPC sockets
+  ========================= }
 
+function SocketCanRead(Sock: TSocketHandle; TimeoutMs: LongInt): Boolean;
+var
+  FDSet: TFDSet;
+  TV: TTimeVal;
+  Res: cint;
+begin
+  Result := False;
+  if Sock = INVALID_SOCKET then Exit;
+
+  fpFD_ZERO(FDSet);
+  fpFD_SET(Sock, FDSet);
+
+  TV.tv_sec  := TimeoutMs div 1000;
+  TV.tv_usec := (TimeoutMs mod 1000) * 1000;
+
+  Res := fpSelect(Sock + 1, @FDSet, nil, nil, @TV);
+  Result := (Res > 0) and (fpFD_ISSET(Sock, FDSet) <> 0);
+end;
+
+function SocketRecvPacket(Sock: TSocketHandle; TimeoutMs: LongInt): string;
+const
+  BUF_SIZE = 4096;
+var
+  Buf: array[0..BUF_SIZE - 1] of Byte;
+  R: ssize_t;
+begin
+  Result := '';
+  if not SocketCanRead(Sock, TimeoutMs) then Exit;
+
+  R := fpRecv(Sock, @Buf[0], BUF_SIZE, 0);
+  if R <= 0 then Exit; // connection closed or error
+
+  SetLength(Result, R);
+  Move(Buf[0], Result[1], R);
+end;
+
+function SocketSendString(Sock: TSocketHandle; const S: string): Boolean;
+var
+  SentTotal, ToSend: SizeInt;
+  R: ssize_t;
+begin
+  Result := False;
+  if Sock = INVALID_SOCKET then Exit;
+  if S = '' then Exit(True);
+
+  SentTotal := 0;
+  ToSend := Length(S);
+  while SentTotal < ToSend do
+  begin
+    R := fpSend(Sock, @S[SentTotal + 1], ToSend - SentTotal, 0);
+    if R <= 0 then Exit; // error
+    Inc(SentTotal, R);
+  end;
+  Result := True;
+end;
+
+procedure CloseSocketSafe(var Sock: TSocketHandle);
+begin
+  if Sock <> INVALID_SOCKET then
+  begin
+    fpClose(Sock);
+    Sock := INVALID_SOCKET;
+  end;
+end;
+
+function SocketGetPeerInfo6(Sock: TSocketHandle; out IP: string; out Port: Word): Boolean;
+var
+  Addr: TInetSockAddr6;
+  Len: TSockLen;
+begin
+  IP := '';
+  Port := 0;
+  Len := SizeOf(Addr);
+  FillChar(Addr, Len, 0);
+  Result := (fpGetPeerName(Sock, @Addr, @Len) = 0);
+  if Result then
+  begin
+    IP   := HostAddrToStr6(Addr.sin6_addr);
+    Port := ntohs(Addr.sin6_port);
+  end;
+end;
 
 { TReceiverThread }
 
-constructor TReceiverThread.Create(ASocket: TTCPBlockSocket; AMemo: TMemo);
+constructor TReceiverThread.Create(ASocket: TSocketHandle; AMemo: TMemo);
 begin
   inherited Create(False);     //start thread
   FreeOnTerminate := True;
@@ -146,55 +233,52 @@ var
 begin
   while not Terminated do
   begin
-    // wait until there is data to read, timeout every 100
-    if FSocket.CanRead(100) then
+    // wait until there is data to read
+    if SocketCanRead(FSocket, 100) then
     begin
-      chunk := FSocket.RecvPacket(100);
+      chunk := SocketRecvPacket(FSocket, 100);
 
-      if FSocket.LastError <> 0 then
-        Break;
+      if chunk = '' then
+        Break; // closed or error
 
-      if chunk <> '' then
+      // if we got data, buffer and split lines
+      FBuf := FBuf + chunk;
+      FLines.Clear;
+
+      // CRLF to LF
+      FBuf := StringReplace(FBuf, #13#10, #10, [rfReplaceAll]);
+
+      while True do
       begin
-        // if we got data, buffer and split lines
-        FBuf := FBuf + chunk;
-        FLines.Clear;
-
-        // CRLF to LF
-        FBuf := StringReplace(FBuf, #13#10, #10, [rfReplaceAll]);
-
-        while True do
-        begin
-          p := Pos(#10, FBuf);
-          if p = 0 then Break;
-          line := Copy(FBuf, 1, p - 1);
-          Delete(FBuf, 1, p); // remove line + LF
-          FLines.Add(line);
-        end;
-
-        if FLines.Count > 0 then
-          Synchronize(@AppendLines);
+        p := Pos(#10, FBuf);
+        if p = 0 then Break;
+        line := Copy(FBuf, 1, p - 1);
+        Delete(FBuf, 1, p); // remove line + LF
+        FLines.Add(line);
       end;
+
+      if FLines.Count > 0 then
+        Synchronize(@AppendLines);
     end;
   end;
 end;
 
 
 { TAcceptorThread }
+
 constructor TAcceptorThread.Create(AOwner: TForm1; const APort: string);
 begin
   inherited Create(False);
   FreeOnTerminate := False;
   FOwner := AOwner;
   FPort := APort;
-  FListenSock := TTCPBlockSocket.Create;
-  FAcceptedSock := nil;
+  FListenSock := INVALID_SOCKET;
+  FAcceptedSock := INVALID_SOCKET;
 end;
 
 destructor TAcceptorThread.Destroy;
 begin
-  if Assigned(FListenSock) then
-    FListenSock.Free;
+  CloseSocketSafe(FListenSock);
   inherited Destroy;
 end;
 
@@ -202,68 +286,82 @@ procedure TAcceptorThread.StopListening;
 begin
   Terminate;
   try
-    if Assigned(FListenSock) then
-      FListenSock.CloseSocket;
+    if FListenSock <> INVALID_SOCKET then
+      fpShutdown(FListenSock, 2); // wake select/accept
   except
   end;
 end;
 
 procedure TAcceptorThread.Execute;
 var
-  s: TSocket;
+  Addr6: TInetSockAddr6;
+  ClientAddr6: TInetSockAddr6;
+  Len: TSockLen;
+  ClientSock: TSocketHandle;
+  Opt: cint;
 begin
-  FListenSock.CreateSocket;
-  FListenSock.SetLinger(True, 1000);
+  try
+    // Pure IPv6 socket
+    FListenSock := fpSocket(AF_INET6, SOCK_STREAM, 0);
+    if FListenSock = INVALID_SOCKET then Exit;
 
-  // Try IPv4 first (simpler for testing)
-  FListenSock.Family := SF_IP4;
-  FListenSock.Bind('127.0.0.1', FPort);
+    //
+    Opt := 1;
+    fpSetSockOpt(FListenSock, IPPROTO_IPV6, IPV6_V6ONLY, @Opt, SizeOf(Opt));
 
-  if FListenSock.LastError <> 0 then
-  begin
-    // Can't log directly, but thread will exit
-    Exit;
-  end;
+    // Bind to :: on specified port
+    FillChar(Addr6, SizeOf(Addr6), 0);
+    Addr6.sin6_family := AF_INET6;
+    Addr6.sin6_port   := htons(StrToIntDef(FPort, 0));
+    Addr6.sin6_addr   := StrToHostAddr6('::'); // all IPv6 interfaces
 
-  FListenSock.Listen;
+    if fpBind(FListenSock, @Addr6, SizeOf(Addr6)) <> 0 then
+      Exit;
 
-  if FListenSock.LastError <> 0 then
-    Exit;
+    if fpListen(FListenSock, 5) <> 0 then
+      Exit;
 
-  while not Terminated do
-  begin
-    if FListenSock.CanRead(1000) then
+    //if Assigned(FOwner) then
+      //FOwner.Memo1.Lines.Add('Listening [IPv6] on port ' + FPort);
+
+    while not Terminated do
     begin
-      s := FListenSock.Accept;
-      if s <> INVALID_SOCKET then
+      Len := SizeOf(ClientAddr6);
+      ClientSock := fpAccept(FListenSock, @ClientAddr6, @Len);
+      if ClientSock >= 0 then
       begin
-        FAcceptedSock := TTCPBlockSocket.Create;
-        FAcceptedSock.Socket := s;
+        FAcceptedSock := ClientSock;
         Synchronize(@SyncAccepted);
-        // Break after first connection (optional)
-        // Break;
+        FAcceptedSock := INVALID_SOCKET;
+      end
+      else
+      begin
+        if Terminated then Break;
+        Sleep(10);
       end;
     end;
+  except
+    on E: Exception do
+      if Assigned(FOwner) then
+        FOwner.Memo1.Lines.Add('Listener error: ' + E.Message);
   end;
+
+  if Assigned(FOwner) then
+    FOwner.Memo1.Lines.Add('Listener stopped');
 end;
 
 procedure TAcceptorThread.SyncAccepted;
 begin
-  if Assigned(FOwner) and Assigned(FAcceptedSock) then
-  begin
+  if Assigned(FOwner) and (FAcceptedSock <> INVALID_SOCKET) then
     FOwner.AttachIncomingSocket(FAcceptedSock);
-    FAcceptedSock := nil; // owner now owns the socket
-  end;
 end;
 
 
 { TForm1 }
 
-//SOCKETS START
-
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  FSocket := nil;
+  FSocket := INVALID_SOCKET;
   FReceiverThread := nil;
   FAcceptorThread := nil;
   FConnected := False;
@@ -296,19 +394,22 @@ begin
     FReceiverThread := nil;
   end;
 
-  if Assigned(FSocket) then
-  begin
-    FSocket.Free;
-    FSocket := nil;
-  end;
+  CloseSocketSafe(FSocket);
 end;
 
-procedure TForm1.AttachIncomingSocket(ASocket: TTCPBlockSocket);
+procedure TForm1.AttachIncomingSocket(ASocket: TSocketHandle);
+var
+  ip: string;
+  p: Word;
 begin
+  CloseSocketSafe(FSocket);
   FSocket := ASocket;
   FConnected := True;
 
-  Memo1.Lines.Add('Միացում ստացվեց։');
+  if SocketGetPeerInfo6(FSocket, ip, p) then
+    Memo1.Lines.Add('Միացում ստացվեց՝ [' + ip + ']:' + IntToStr(p))
+  else
+    Memo1.Lines.Add('Միացում ստացվեց։');
 
   FReceiverThread := TReceiverThread.Create(FSocket, Memo1);
   FAcceptorThread := nil; // we stop listening once connected
@@ -317,14 +418,12 @@ begin
 end;
 
 
-
-//SOCKETS END
-
-
-
-//BUDDIES HANDELING   START
+{ ============= BUDDIES HANDLING ============= }
 
 procedure TForm1.ConnectToHost(const Host, PortStr: string);
+var
+  Addr6: TInetSockAddr6;
+  PortNum: Integer;
 begin
   if Host = '' then
   begin
@@ -344,25 +443,40 @@ begin
     Exit;
   end;
 
-  FSocket := TTCPBlockSocket.Create;
-
-  FSocket.Connect(Host, PortStr);
-
-  if FSocket.LastError = 0 then
+  PortNum := StrToIntDef(PortStr, 0);
+  if PortNum <= 0 then
   begin
-    FConnected := True;
-    Memo1.Lines.Add('Միացա ' + Host + ':' + PortStr);
-    FReceiverThread := TReceiverThread.Create(FSocket, Memo1);
-    UpdateButtons;
-  end
-  else
-  begin
-    Memo1.Lines.Add('Սխալ կապի ժամանակ: ' + FSocket.LastErrorDesc);
-    FSocket.Free;
-    FSocket := nil;
+    Memo1.Lines.Add('Սխալ պորտի համար։');
+    Exit;
   end;
-end;
 
+  FSocket := fpSocket(AF_INET6, SOCK_STREAM, 0);
+  if FSocket = INVALID_SOCKET then
+  begin
+    Memo1.Lines.Add('Չստացվեց ստեղծել սոկետ։');
+    Exit;
+  end;
+
+  FillChar(Addr6, SizeOf(Addr6), 0);
+  Addr6.sin6_family := AF_INET6;
+  Addr6.sin6_port   := htons(PortNum);
+
+  //  expect literal IPv6 address in Edit1 - maybe chnage later
+  Addr6.sin6_addr := StrToHostAddr6(Host);
+
+  if fpConnect(FSocket, @Addr6, SizeOf(Addr6)) <> 0 then
+  begin
+    Memo1.Lines.Add('Սխալ կապի ժամանակ։');
+    CloseSocketSafe(FSocket);
+    Exit;
+  end;
+
+  FConnected := True;
+  Memo1.Lines.Add('Միացա [' + Host + ']:' + PortStr);
+
+  FReceiverThread := TReceiverThread.Create(FSocket, Memo1);
+  UpdateButtons;
+end;
 
 procedure TForm1.lstBuddiesDblClick(Sender: TObject);
 var
@@ -377,12 +491,12 @@ begin
   Edit1.Text := B.Host;
   port.Text  := B.Port;
 
-  ConnectToHost(B.Host, B.Port); // your existing connect helper
+  ConnectToHost(B.Host, B.Port);
 end;
 
 function TForm1.GetConfigFileName: string;
 begin
-    Result := ExtractFilePath(ParamStr(0)) + 'buddies.conf';
+  Result := ExtractFilePath(ParamStr(0)) + 'buddies.conf';
 end;
 
 procedure TForm1.RefreshBuddyList;
@@ -393,36 +507,30 @@ begin
   for i := 0 to FBuddyManager.CountBuddies - 1 do
     lstBuddies.Items.Add(FBuddyManager.GetDisplayName(i));
 end;
-//BUDDIES HANDELING     END
 
 
-
-
-//BUTTONS    START
+{ ============= BUTTONS ============= }
 
 procedure TForm1.Button1Click(Sender: TObject); // add buddy
 var
   B: TBuddy;
   Buddyname: string;
 begin
-  // Initialize empty
   B.Host := '';
   B.Port := '';
   B.Name := '';
 
-  // Ask for Host/IP
-  if not InputQuery('Ավելացնել տնեցիք', 'IP/Հասցե:', B.Host) then
+  if not InputQuery('Ավելացնել տնեցի', 'IPv6 հասցե:', B.Host) then
     Exit;
 
   B.Host := Trim(B.Host);
   if B.Host = '' then
   begin
-    ShowMessage('Մուտքագրի՛ր IP/հասցեն։');
+    ShowMessage('Մուտքագրի՛ր IPv6 հասցեն։');
     Exit;
   end;
 
-  // Ask for Port
-  if not InputQuery('Ավելացնել տնեցիք', 'Պորտ:', B.Port) then
+  if not InputQuery('Ավելացնել տնեցի', 'Պորտ:', B.Port) then
     Exit;
 
   B.Port := Trim(B.Port);
@@ -432,19 +540,17 @@ begin
     Exit;
   end;
 
-  // Ask for Display Name (pre-filled with host)
   Buddyname := B.Host;
-  if not InputQuery('Ավելացնել տնեցիք', 'Անուն (ցուցադրվող):', Buddyname) then
+  if not InputQuery('Ավելացնել տնեցի', 'Անուն (ցուցադրվող):', Buddyname) then
     Exit;
 
   B.Name := Trim(Buddyname);
 
-  // Add and save
   FBuddyManager.AddBuddy(B);
   FBuddyManager.Save;
   RefreshBuddyList;
 
-  Memo1.Lines.Add('Տնեցիքը ավելացվեց: ' + B.Name);
+  Memo1.Lines.Add('Տնեցին ավելացվեց: ' + B.Name);
 end;
 
 procedure TForm1.Button6Click(Sender: TObject);
@@ -465,7 +571,7 @@ end;
 
 procedure TForm1.Button3Click(Sender: TObject);
 var
-  Message: string;
+  MessageStr: string;
 begin
   if not FConnected then
   begin
@@ -473,20 +579,16 @@ begin
     Exit;
   end;
 
-  Message := Trim(Edit2.Text);
-  if Message = '' then Exit;
+  MessageStr := Trim(Edit2.Text);
+  if MessageStr = '' then Exit;
 
-  FSocket.SendString(Message + #13#10);
-
-  if FSocket.LastError = 0 then
+  if SocketSendString(FSocket, MessageStr + #13#10) then
   begin
-    Memo1.Lines.Add('► Ուղարկված: ' + Message);
+    Memo1.Lines.Add('► Ուղարկված: ' + MessageStr);
     Edit2.Clear;
   end
   else
-  begin
-    Memo1.Lines.Add('Սխալ ուղարկելու ժամանակ: ' + FSocket.LastErrorDesc);
-  end;
+    Memo1.Lines.Add('Սխալ ուղարկելու ժամանակ։');
 end;
 
 procedure TForm1.Button5Click(Sender: TObject);
@@ -503,7 +605,7 @@ begin
   if PortStr = '' then
     PortStr := '8080';
 
-  Memo1.Lines.Add('Լսում եմ պորտի վրա ' + PortStr + ' ...');
+  Memo1.Lines.Add('Լսում եմ [IPv6] պորտի վրա ' + PortStr + ' ...');
 
   FAcceptorThread := TAcceptorThread.Create(Self, PortStr);
   UpdateButtons;
@@ -526,18 +628,14 @@ begin
     FReceiverThread := nil;
   end;
 
-  if Assigned(FSocket) then
-  begin
-    FSocket.Free;
-    FSocket := nil;
-  end;
-
+  CloseSocketSafe(FSocket);
   FConnected := False;
 
   if Assigned(FAcceptorThread) then
   begin
-    FAcceptorThread.Terminate;
-    FAcceptorThread := nil;
+    FAcceptorThread.StopListening;
+    FAcceptorThread.WaitFor;
+    FreeAndNil(FAcceptorThread);
   end;
 
   UpdateButtons;
@@ -555,31 +653,24 @@ begin
   Button6.Enabled := lstBuddies.ItemIndex >= 0;            // remove buddy if selected
 end;
 
-//BUTTONS END
 
+{ ============= OTHER UI HANDLERS ============= }
 
-//OTHER UI COMPONENTS START
 procedure TForm1.Edit1Change(Sender: TObject);
 begin
-
 end;
 
 procedure TForm1.Memo1Change(Sender: TObject);
 begin
-
 end;
 
 procedure TForm1.portChange(Sender: TObject);
 begin
-
 end;
 
 procedure TForm1.Label1Click(Sender: TObject);
 begin
-
 end;
-
-//COMPONENTS END
 
 end.
 
